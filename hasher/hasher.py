6ccee9c8886397243d5bc25b3b6b9929abbb92c7
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2025-09-19 16:37:33 +0000

    Updates compose file for hasher services

---
+++ hasher/hasher.py
@@ -0,0 +1,46 @@
+import json
+from datetime import datetime, timezone
+import resource
+from PasswordHasher import PasswordHasher
+from utils import pickle_object
+
+
+
+
+if __name__ == "__main__":
+    # json_file = json.load(open("parameters.json", "r"))
+
+    json_file = pickle_object('-', 'parameters.pkl', mode='load')
+    start_time_utc = datetime.now(timezone.utc)
+    resource_usage_start = resource.getrusage(resource.RUSAGE_SELF)
+    
+    hasher = PasswordHasher(algorithm=json_file['algorithm'], **json_file['parameters'])
+    salt, generated_hash = hasher.generate_hash(json_file['password_plaintext'])
+    end_time_utc = datetime.now(timezone.utc)
+    resource_usage_end = resource.getrusage(resource.RUSAGE_SELF)
+
+    memory_rss_mb_start = resource_usage_start.ru_maxrss / 1024
+    cpu_user_time_ms_start = resource_usage_start.ru_utime * 1000
+    cpu_system_time_ms_start = resource_usage_start.ru_stime * 1000
+
+    cpu_user_time_ms_end = resource_usage_end.ru_utime * 1000
+    cpu_system_time_ms_end = resource_usage_end.ru_stime * 1000
+    memory_peak_mb_during_hash = resource_usage_end.ru_maxrss / 1024
+
+    cpu_system_time_ms = cpu_system_time_ms_end - cpu_system_time_ms_start
+    cpu_user_time_ms = cpu_user_time_ms_end - cpu_user_time_ms_start
+    duration_ms = (end_time_utc - start_time_utc).total_seconds() * 1000
+
+    # results = json.load(open("results.json", "r"))
+    results = pickle_object('-', 'results.pkl', mode='load')
+    results.update({
+        "start_time_utc": start_time_utc.isoformat(),
+        "end_time_utc": end_time_utc.isoformat(),
+        "duration_ms": duration_ms,
+        "cpu_user_time_ms": cpu_user_time_ms,
+        "cpu_system_time_ms": cpu_system_time_ms,
+        "memory_rss_mb_start": memory_rss_mb_start,
+        "memory_peak_mb_during_hash": memory_peak_mb_during_hash
+    })
+    # json.dump(results, open("results.json", "w"))
+    pickle_object(results, 'results.pkl', mode='save')
---
+++ hasher/utils.py
@@ -0,0 +1,92 @@
+from sqlalchemy import create_engine
+
+
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        count = 1
+        words = []
+        try:
+            for l in f:
+                if len(l) > 150:
+                    continue
+                count += 1
+                words.append(l.strip())
+        except UnicodeDecodeError as e:
+            pass
+            
+    return words
+
+        
+def create_db_connection(user, password, host, port, database):
+    """
+    Creates a SQLAlchemy database connection.
+
+    Args:
+        user (str): Database username.
+        password (str): Database password.
+        host (str): Database host address.
+        port (int): Database port number.
+        database (str): Database name.
+
+    Returns:
+        sqlalchemy.engine.base.Connection: A SQLAlchemy connection object.
+    """
+    try:
+        engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+        connection = engine.connect()
+        print("Database connection established.")
+        return connection
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        return None
+    
+    
+def get_db_password(secret_path='/run/secrets/db_password'):
+    """
+    Reads the database password from a secure file.
+
+    Args:
+        secret_path (str): Path to the file containing the database password.
+
+    Returns:
+        str: The database password.
+    """
+    try:
+        with open(secret_path, 'r') as f:
+            db_password = f.read().strip()
+        return db_password
+    except Exception as e:
+        print(f"Error reading database password from '{secret_path}': {e}")
+        exit(1)
+
+def db_query_generator(conn, query):
+    """
+    A generator function that yields rows from a database query.
+    
+    Args:
+        conn: A database connection object.
+        query: The SQLAlchemy Select query.
+    
+    Yields:
+        A Row object representing a single row from the result set.
+    """
+    try:
+        result = conn.execute(query)
+        for row in result:
+            yield row
+    except Exception as e:
+        print(f"Database error: {e}")
+        conn.rollback()
+        raise
+
+
+def pickle_object(obj, file_path, mode='save'):
+    import pickle
+    if mode == 'save':
+        with open(file_path, 'wb') as f:
+            pickle.dump(obj, f)
+    elif mode == 'load':
+        with open(file_path, 'rb') as f:
+            return pickle.load(f)
+    else:
+        raise ValueError("Mode should be either 'save' or 'load'.")
